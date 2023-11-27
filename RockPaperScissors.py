47e61cabe1fc217b3ad3ae6909c43291746ff887
Amelia Sinclaire <asinclaire.hri@gmail.com>
2023-11-27 06:19:44 -0500

    Began breaking AI "Strategies" into own file -- created way to automatically counter any defined strategy. (needs fine-tuning)

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -3,7 +3,8 @@ import random
 from enum import Enum
 import numpy as np
 import logging
-logging.basicConfig(format='%(message)s', level=logging.ERROR)
+logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+import Strategies
 
 State = Enum('State', ['HUMAN_WINS', 'COMPUTER_WINS', 'TIE'])
 Throws = Enum('Throw', ['ROCK', 'PAPER', 'SCISSORS', 'EXIT'])
@@ -11,6 +12,9 @@ Throws = Enum('Throw', ['ROCK', 'PAPER', 'SCISSORS', 'EXIT'])
 
 class Rounds:
     rounds = []
+
+    def __init__(self, rounds=None):
+        Rounds.rounds = rounds if rounds is not None else []
 
     def add_round(self, r):
         self.rounds.append(r)
@@ -380,39 +384,48 @@ def validate_throw(t):
 
 
 def evaluate_game(p1, p2):
-    if p1 == p2:
+    if p1.value == p2.value:
         return State['TIE']
-    if p1 == Throws['ROCK']:
-        if p2 == Throws['PAPER']:
+    if p1.value == Throws['ROCK'].value:
+        if p2.value == Throws['PAPER'].value:
             return State['COMPUTER_WINS']
         return State['HUMAN_WINS']
-    if p1 == Throws['PAPER']:
-        if p2 == Throws['SCISSORS']:
+    if p1.value == Throws['PAPER'].value:
+        if p2.value == Throws['SCISSORS'].value:
             return State['COMPUTER_WINS']
         return State['HUMAN_WINS']
-    if p2 == Throws['ROCK']:
+    if p2.value == Throws['ROCK'].value:
         return State['COMPUTER_WINS']
     return State['HUMAN_WINS']
 
 
 def main(all_rounds):
-    # Get Computer Throw
-    p2_throw = smart_throw(all_rounds)
+    computer = Strategies.CounterCopy()
 
-    # Get Player Throw
-    p1_throw = None
-    while p1_throw is None:
-        p1_throw = input("What will you Throw? > ")
-        p1_throw = validate_throw(p1_throw)
-    if p1_throw == Throws['EXIT']:
-        print('exiting program')
-        exit()
-    this_round = Round(p1_throw, p2_throw)
-    this_round.display_round()
-    all_rounds.add_round(this_round)
-    # all_rounds.display_rounds()
-    all_rounds.display_percentages()
-    main(all_rounds)
+    def loop(all_rounds):
+        # Get Computer Throw
+        # p2_throw = smart_throw(all_rounds)
+        p2_throw = computer.throw()
+
+        # Get Player Throw
+        p1_throw = None
+        while p1_throw is None:
+            p1_throw = input("What will you Throw? > ")
+            p1_throw = validate_throw(p1_throw)
+        if p1_throw == Throws['EXIT']:
+            print('exiting program')
+            exit()
+        this_round = Round(p1_throw, p2_throw)
+        this_round.display_round()
+        all_rounds.add_round(this_round)
+        # all_rounds.display_rounds()
+        all_rounds.display_percentages()
+
+        computer.update(all_rounds)
+
+        loop(all_rounds)
+
+    loop(all_rounds)
 
 
 if __name__ == "__main__":
---
+++ Strategies.py
@@ -0,0 +1,110 @@
+# Amelia Sinclaire 2023
+import random
+from RockPaperScissors import Throws, Rounds
+import logging
+logging.basicConfig(format='%(message)s', level=logging.INFO)
+
+
+class Strategy:
+    def __init__(self, computer=True, rounds=None):
+        self.computer = computer
+        self.rounds = rounds if rounds is not None else Rounds()
+
+    def update(self, rounds):
+        self.rounds = rounds
+
+    def throw(self):
+        pass
+
+    @staticmethod
+    def counter_throw(throw):
+        if throw == Throws['ROCK']:
+            return Throws['PAPER']
+        if throw == Throws['PAPER']:
+            return Throws['SCISSORS']
+        return Throws['ROCK']
+
+    def counter_strat(self, strat, lookback=10, weight=1, consecutive_freq_thresh=0.15, frequency_thresh=0.6):
+        if self.rounds.empty():  # no info = no confidence
+            return {
+                "opponent_next_throw": Throws['PAPER'],
+                "frequency": 0,
+                "consecutive_frequency": 0,
+                "consecutive": 0,
+                "confidence": 0
+            }
+        # for previous `lookback` number of rounds
+        # see how many of them the given strat was employed by opp
+        strat_count = 0
+        consecutive = 0
+        consecutive_done = False
+        for i in range(0, lookback):
+            n = 0 if lookback is None else -lookback
+            if i == 0:
+                lookback_rounds = self.rounds.rounds[n - i:]
+            else:
+                lookback_rounds = self.rounds.rounds[n - i:-i]
+            if len(lookback_rounds) == 0:
+                continue
+            current_round = lookback_rounds[-1]
+            del lookback_rounds[-1]
+
+            generator = strat(computer=(not self.computer), rounds=Rounds(lookback_rounds))
+            expected = generator.throw()
+            if self.computer:
+                actual = current_round.p1
+            else:
+                actual = current_round.p2
+
+            if expected.value == actual.value:
+                # print('*bing!*')
+                strat_count += 1
+                if not consecutive_done:
+                    consecutive += 1
+                continue
+            consecutive_done = True
+
+        frequency = strat_count / lookback
+        consecutive_frequency = consecutive / lookback
+        g = strat(computer=False, rounds=self.rounds)
+        logging.info(f'{g.__class__.__name__} FREQUENCY: {frequency} CONSECUTIVE_FREQ: {consecutive_frequency}')
+        confidence = frequency  # (consecutive_frequency + frequency)/2
+        if consecutive > consecutive_freq_thresh:
+            confidence *= 1.8  # reward if we have been more frequent than thresh
+        if frequency < frequency_thresh:
+            confidence *= 0.5  # penalize if hasn't been consistent
+        confidence = min(confidence, 1)
+        opponent_next_throw = g.throw()
+        logging.info(f'OPPONENT NEXT THROW : {opponent_next_throw} CONFIDENCE: {confidence}')
+
+        output = {
+            "opponent_next_throw": opponent_next_throw,
+            "frequency": frequency,
+            "consecutive_frequency": consecutive_frequency,
+            "consecutive": consecutive,
+            "confidence": confidence
+        }
+        return output
+
+
+class RandomStrat(Strategy):
+    def throw(self):
+        return random.choice(list(Throws)[0:3])  # random throw
+
+
+class CopyStrat(Strategy):
+    def throw(self):
+        if self.rounds.empty():
+            return random.choice(list(Throws)[0:3])  # random throw
+        if self.computer:
+            return self.rounds.rounds[-1].p1  # copy human's last move
+        return self.rounds.rounds[-1].p2  # copy computer's last move
+
+
+class CounterCopy(Strategy):
+    def throw(self):
+        result = self.counter_strat(CopyStrat, lookback=10, weight=1)
+        if result["consecutive"] >= 3:
+            return self.counter_throw(result["opponent_next_throw"])
+        else:
+            return random.choice(list(Throws)[0:3])  # random throw
